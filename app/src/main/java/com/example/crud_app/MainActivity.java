7c095196fe1b7291dc31741edb8f8161d142179b
hp <tararhanzla783@gmail.co,m>
2023-07-09 05:56:22 -0700

    I have completed the basic functionalities of SurahManager

---
+++ app/src/main/java/com/example/crud_app/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.crud_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> surahList= new ArrayList<>();
+    ListView surahlist;
+    EditText ET_surah;
+    Button addSurahBtn;
+    Button deleteSurahBtn;
+    Button updateSurahBtn;
+    int position=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        surahlist=findViewById(R.id.surahList);
+        deleteSurahBtn=findViewById(R.id.deleteSurahBtn);
+        updateSurahBtn=findViewById(R.id.updateSurahBtn);
+       surahList.add("Furqan");
+       surahList.add("Baqra");
+       surahList.add("Annaas");
+       surahList.add("falak");
+
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,surahList);
+        surahlist.setAdapter(arrayAdapter);
+
+        addSurahBtn=findViewById(R.id.addSurah);
+        ET_surah=findViewById(R.id.ET_surah);
+        Collections.sort(surahList);
+
+        addSurahBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                surahList.add(ET_surah.getText().toString());
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+
+        surahlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                ET_surah.setText(surahList.get(i));
+                position=i;
+
+            }
+        });
+
+      deleteSurahBtn.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              surahList.remove(position);
+              arrayAdapter.notifyDataSetChanged();
+          }
+      });
+
+      updateSurahBtn.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              // here implement update functionalities
+              surahList.set(position,ET_surah.getText().toString());
+              arrayAdapter.notifyDataSetChanged();
+          }
+      });
+
+
+    }
+}
